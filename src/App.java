bb156a17d5e4230bea3f079e91da861028667a24
Felipe Garcia <felipegarc.tech@gmail.com>
2023-03-31 00:36:47 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,44 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+        
+        URI endereco = URI.create(url);
+        
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        
+        String body = response.body();
+
+        System.out.println(body);
+
+        // extrair só os dado que interessam (título, poster, classificção)
+        var parser = new JsonParser();
+
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("\u001b[1m{title}"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+            
+        }
+        System.out.println("\u001b[1m Alura + Java");
+
+    }
+}
